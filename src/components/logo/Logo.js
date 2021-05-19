5e513f2273d631b8fa653c24193a1abbe08a0b88
Osama Islam <islamosama488@gmail.com>
2021-05-19 22:14:15 +0530

    fixing image issues

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -8,7 +8,7 @@ const Logo = () => {
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max: 65 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner pd3 ">
-                    <img src={brain} alt='brain-logo' />
+                    <img src={brain} alt='brain-logo' id='brain' />
                 </div>
             </Tilt>
         </div>
